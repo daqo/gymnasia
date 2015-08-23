8fa563df74ce6707443a97539e834960b26f0cd7
David Qorashi <davoud.qorashi@gmail.com>
2015-08-22 17:00:50 -0700

    Switch to PostgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150822224556) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "machine_types", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
